e517758e63a0b675d8815fc062448c9257e9cd37
parag.majithia <parag.majithia@trafigura.com>
2022-06-14 17:46:11 +0530

    Initial demo Spring boot app

---
+++ demo_web/src/main/java/com/parag/spring/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.parag.spring.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String greet(@RequestParam(name = "myName", required = false, defaultValue = "unknown user")
+                                    String myName) {
+        return String.format("Hello %s", myName);
+    }
+}
---
+++ demo_web/src/main/java/com/parag/spring/demo/model/User.java
@@ -0,0 +1,6 @@
+package com.parag.spring.demo.model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+}
